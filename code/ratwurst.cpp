9bd11423011b2af0394cf85068956f709dcf5f4d
AccidentalRebel <accidentalrebel@gmail.com>
2021-07-01 14:08:57 +0800

    Added more socket functions.

--- code/ratwurst.cpp
+++ code/ratwurst.cpp
@@ -5,6 +5,12 @@
 #define _SOCKET(name) SOCKET WSAAPI name(int af, int type, int protocol);
 typedef _SOCKET(_socket);
 
+#define _CONNECT(name) int WSAAPI name(SOCKET s, const sockaddr *name, int namelen);
+typedef _CONNECT(_connect);
+
+#define _SEND(name) int WSAAPI name(SOCKET s, const char *buf, int len, int flags);
+typedef _SEND(_send);
+
 int CALLBACK
 WinMain(HINSTANCE hInstance,
 		HINSTANCE hPrevInstance,
@@ -24,9 +30,9 @@ WinMain(HINSTANCE hInstance,
         return 1;
     }
 
-	SOCKET socketConnection;
-
 	_socket* fSocket = (_socket*)GetProcAddress(libraryWinsock2, "socket");
+
+	SOCKET socketConnection;
 	socketConnection = fSocket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if ( socketConnection == INVALID_SOCKET )
 	{	
@@ -43,7 +49,8 @@ WinMain(HINSTANCE hInstance,
 	socketAddress.sin_port = htons(65432);
 	socketAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-	if ( connect(socketConnection, (SOCKADDR*) &socketAddress, sizeof(socketAddress)) == SOCKET_ERROR )
+	_connect* fConnect = (_connect*)GetProcAddress(libraryWinsock2, "connect");
+	if ( fConnect(socketConnection, (SOCKADDR*) &socketAddress, sizeof(socketAddress)) == SOCKET_ERROR )
 	{
 		char buffer[256];
 		sprintf(buffer, "Socket failed with error: %ld\nReconnecting...", WSAGetLastError());
@@ -54,9 +61,10 @@ WinMain(HINSTANCE hInstance,
 		WSACleanup();
 		Sleep(2000);
 	}
-	
+
+	_send* fSend = (_send*)GetProcAddress(libraryWinsock2, "send");
 	const char* socketBuffer = "THIS IS A TEST.";
-	if ( send(socketConnection, socketBuffer, (int)strlen(socketBuffer), 0) == SOCKET_ERROR )
+	if ( fSend(socketConnection, socketBuffer, (int)strlen(socketBuffer), 0) == SOCKET_ERROR )
 	{
 		char buffer[256];
 		sprintf(buffer, "Socket failed with error: %ld\n", WSAGetLastError());
